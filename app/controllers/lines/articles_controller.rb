22204a98a3f048c995108ee8eb8653bc18fcb2b6
Rahul Nawani <rahul@samplytics.com>
2017-10-27 15:57:42 +0530

    don't leave first article in articles list

--- app/controllers/lines/articles_controller.rb
+++ app/controllers/lines/articles_controller.rb
@@ -20,7 +20,7 @@ module Lines
           if params[:tag]
             @articles = Article.published.tagged_with(params[:tag]).page(params[:page].to_i)
           else
-            @articles = Article.published.page(params[:page].to_i).padding(1)
+            @articles = Article.published.page(params[:page].to_i)
           end
           
           if @articles.first_page?
