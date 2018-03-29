7e90cb9995a2aaa54af8c85df3493d5c3a1a080f
Rahul Nawani <rahul@samplytics.com>
2018-03-29 19:28:09 +0530

    fixing for rails 5.1

--- lib/lines/engine.rb
+++ lib/lines/engine.rb
@@ -21,7 +21,7 @@ module Lines
 
     # Initializer to combine this engines static assets with the static assets of the hosting site.
     initializer "static assets" do |app|
-      if Rails.application.config.serve_static_assets
+      if Rails.application.config.public_file_server.enabled
         app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
       end
     end
